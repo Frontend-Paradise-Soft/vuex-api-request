60e0b4e0bba3ce1e3a5caf9764db0c6c3ca30a4e
YW-Tseng <ywwwtseng@gmail.com>
2019-01-10 17:21:37 +0800

    fix authLocalStoragePlugin

--- src/auth/authLocalStoragePlugin.js
+++ src/auth/authLocalStoragePlugin.js
@@ -1,14 +1,12 @@
-import auth from './store'
-
 // LocalStorage plugin.
 const AuthLocalStoragePlugin = ({storageKey = 'paradise-soft'}) => {
-  // Sync with local storage.
-  if (localStorage.getItem(constants.STORAGE_KEY)) {
-    const syncedState = JSON.parse(localStorage.getItem(constants.STORAGE_KEY))
-    auth.state = Object.assign(auth.state, syncedState.auth)
-  }
-
   return (store) => {
+    // Sync with local storage.
+    if (localStorage.getItem(constants.STORAGE_KEY)) {
+      const syncedState = JSON.parse(localStorage.getItem(constants.STORAGE_KEY))
+      auth.state = Object.assign(auth.state, syncedState.auth)
+    }
+
     store.subscribe((mutation, state) => {
       const syncedData = {auth: state.auth}
   
